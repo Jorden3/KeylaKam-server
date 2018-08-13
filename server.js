e399f396cb3d5c65f7afb80e3064b03cfb3116d4
Jorden Garcia <jorden3@pdx.edu>
2018-08-12 20:42:35 -0700

    html gets images from server, server get image from pi and converts

--- server.js
+++ server.js
@@ -3,12 +3,10 @@
 var express = require('express'); // do not change this line
 var server = express();
 var socket = require('socket.io');
-var fs = require('fs');
 
-
-var spawn = require('child_process').spawn;
 
 var sockets = {};
+var feeder = {};
  
 
 // server.get('/', (req,res)=>{
@@ -16,15 +14,27 @@ var sockets = {};
 // });
 
 server.use('/', express.static(__dirname+'/'));
-var io = socket(server.listen(process.env.PORT || 8080)); // do not change this line
+var io = socket(server.listen(8080)); // do not change this line
 
-io.on('connection', function(socket) {
+
+
+io.on('connect', function(socket) {
  
   sockets[socket.id] = socket;
   console.log("Total clients connected : ", Object.keys(sockets).length);
  
+  socket.on('liveStream', function(image){
+    console.log("I'm getting images");
+    socket.broadcast.emit('stream', "data:image/png;base64,"+ image.toString("base64"))
+  });
+
+  socket.on('feeder',function(){
+    feeder[socket.id] = socket;
+    console.log('hello', socket);
+  });
   socket.on('disconnect', function() {
     delete sockets[socket.id];
+    console.log("Total clients connected : ", Object.keys(sockets).length);
  
     // no more sockets, kill the stream
     if (Object.keys(sockets).length == 0) {
@@ -40,30 +50,3 @@ io.on('connection', function(socket) {
  
 });
 
-function stopStreaming() {
-    if (Object.keys(sockets).length == 0) {
-      app.set('watchingFile', false);
-      if (proc) proc.kill();
-      fs.unwatchFile('./stream/image_stream.jpg');
-    }
-  }
-
-function startStreaming(io) {
- 
-    if (app.get('watchingFile')) {
-      io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-      return;
-    }
-   
-    var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
-    proc = spawn('raspistill', args);
-   
-    console.log('Watching for changes...');
-   
-    app.set('watchingFile', true);
-   
-    fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
-      io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-    })
-   
-  }
